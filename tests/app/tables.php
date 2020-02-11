c3ba9650034bd92233bada1aed3c4e69e64ed289
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-02-11 20:48:22 +0100

    rename http classes

--- tests/app/tables.php
+++ tests/app/tables.php
@@ -521,7 +521,7 @@
     import { Request } from '../../../jsOMS/Message/Request/Request.js';
     import { Table } from '../../../jsOMS/UI/Component/Table.js';
 
-    const loginRequest = new Request('http://127.0.0.1/en/api/login', 'POST', 'json');
+    const loginRequest = new HttpRequest('http://127.0.0.1/en/api/login', 'POST', 'json');
     loginRequest.setData({user: 'admin', pass: 'orange'});
     loginRequest.send();
 
